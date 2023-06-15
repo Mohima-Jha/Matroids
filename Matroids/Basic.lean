2f5aba999907bed6cefad50577af6e7a563df4bd
Mohima <93897060+Mohima-Jha@users.noreply.github.com>
2023-06-15 15:15:14 +0530

    Change of Parameters to Fields

--- Matroids/Basic.lean
+++ Matroids/Basic.lean
@@ -1,6 +1,8 @@
 import Mathlib
 
 structure Matroids (Ground : Finset α) (Independent : Finset Ground → Prop)[DecidableEq α] where
+  {Ground : Finset α}
+  {Independent : Finset Ground → Prop}
   EmptySet : Independent {} 
   Hereditary : ∀ {A B : Finset Ground}, ((Independent A) ∧ (B ⊆ A)) → Independent B 
   Augmentation : ∀ {A B : Finset Ground}, ((Independent A)∧(Independent B)∧(Finset.card B > Finset.card A)) → ∃ e, (e ∈ (B \ A)) ∧ Independent (insert e A) 
