ad58e5c8b1bcb854383debb9dfd03031d0e5844c
Mohima <93897060+Mohima-Jha@users.noreply.github.com>
2023-06-16 11:08:11 +0530

    Rank Function
    1. Defined Rank Function
    2. Added `set_option autoImplicit false`

--- Matroids/Basic.lean
+++ Matroids/Basic.lean
@@ -1,4 +1,6 @@
 import Mathlib
+set_option autoImplicit false
+
 
 structure Matroids (Ground : Finset α) (Independent : Finset Ground → Prop)[DecidableEq α] where
   {Ground : Finset α}
@@ -7,3 +9,9 @@ structure Matroids (Ground : Finset α) (Independent : Finset Ground → Prop)[D
   Hereditary : ∀ {A B : Finset Ground}, ((Independent A) ∧ (B ⊆ A)) → Independent B 
   Augmentation : ∀ {A B : Finset Ground}, ((Independent A)∧(Independent B)∧(Finset.card B > Finset.card A)) → ∃ e, (e ∈ (B \ A)) ∧ Independent (insert e A) 
 
+structure RankFun (α : Type _) [DecidableEq α] where
+  {Ground : Finset α}
+  rank : Finset Ground → ℕ
+  prop1 : ∀ {A : Finset Ground}, rank A ≤ Finset.card A
+  prop2 : ∀ {A B : Finset Ground}, A ⊆ B → rank A ≤ rank B
+  prop3 : ∀ {A B : Finset Ground}, rank (A ∪ B) + rank (A ∩ B) ≤ rank A + rank B
