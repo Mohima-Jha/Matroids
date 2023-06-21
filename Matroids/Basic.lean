76b1a8a14704ef09d91bc1ae853e7be5db664868
Mohima <93897060+Mohima-Jha@users.noreply.github.com>
2023-06-21 15:32:40 +0530

    Circuit

--- Matroids/Basic.lean
+++ Matroids/Basic.lean
@@ -1,7 +1,15 @@
 import Mathlib
 set_option autoImplicit false
 
+--Rank Function
+structure RankFun (α : Type _) [DecidableEq α] where
+  {Ground : Finset α}
+  rank : Finset Ground → ℕ
+  prop1 : ∀ {A : Finset Ground}, rank A ≤ Finset.card A
+  prop2 : ∀ {A B : Finset Ground}, A ⊆ B → rank A ≤ rank B
+  prop3 : ∀ {A B : Finset Ground}, rank (A ∪ B) + rank (A ∩ B) ≤ rank A + rank B
 
+--Matroid
 structure Matroid (α : Type _) [DecidableEq α] where
   {Ground : Finset α}
   {Independent : Finset Ground → Prop}
@@ -10,13 +18,18 @@ structure Matroid (α : Type _) [DecidableEq α] where
   Augmentation' : ∀ {A B : Finset Ground}, ((Independent A)∧(Independent B)∧(Finset.card B > Finset.card A)) → ∃ e, (e ∈ (B \ A)) ∧ Independent (insert e A) 
 
 
-structure RankFun (α : Type _) [DecidableEq α] where
-  {Ground : Finset α}
-  rank : Finset Ground → ℕ
-  prop1 : ∀ {A : Finset Ground}, rank A ≤ Finset.card A
-  prop2 : ∀ {A B : Finset Ground}, A ⊆ B → rank A ≤ rank B
-  prop3 : ∀ {A B : Finset Ground}, rank (A ∪ B) + rank (A ∩ B) ≤ rank A + rank B
-
+-- Dependent
 def Dependent {α : Type _} [DecidableEq α] (M : Matroid α) (A : Finset (M.Ground)) : Prop :=
   ¬ M.Independent A
+
+--Circuit
+def Circuit {α : Type _} [DecidableEq α] (M : Matroid α) (B : Finset (M.Ground)) : Prop :=
+  ∀ (A : Finset (M.Ground)), Dependent M B ∧ (A ⊆ B) → M.Independent A 
+   
+
+      
+
+
+
+
 
