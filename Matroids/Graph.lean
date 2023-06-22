a4ce80dbd8518308b3c1c69ba8e55aed5e09dc94
Mohima <93897060+Mohima-Jha@users.noreply.github.com>
2023-06-22 19:36:46 +0530

    Update Graph.lean

--- Matroids/Graph.lean
+++ Matroids/Graph.lean
@@ -1,26 +1,37 @@
 import Mathlib
 set_option autoImplicit false
 
+--Graph
 structure Graph (Vertex : Type)[DecidableEq Vertex]where 
   Vertices   : List Vertex
   Edges   : List (Vertex × Vertex)
 
+--Loop
 def Loop {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) : Prop :=
   ∃ (V : Vertex), V ∈ G.Vertices ∧ (V, V) ∈ G.Edges
 
+--Adjacent
 def Adjacent {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) (V₁ V₂ : Vertex) : Prop :=
   (V₁,V₂) ∈ G.Edges
 
-inductive Walk {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) : Vertex → Vertex → Prop
-| base : ∀ (V : Vertex), Walk G V V
-| step : ∀ {V₁ V₂ V₃ : Vertex}, Walk G V₁ V₂ → Adjacent G V₂ V₃ → Walk G V₁ V₃
+--Walk
+inductive IsWalk {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) : Vertex → Vertex → Prop
+| base : ∀ (V : Vertex), IsWalk G V V
+| step : ∀ {V₁ V₂ V₃ : Vertex}, IsWalk G V₁ V₂ → Adjacent G V₂ V₃ → IsWalk G V₁ V₃
 
-inductive GPath {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) : Vertex → Vertex → Prop
-| base : ∀ (V : Vertex), GPath G V V
-| step : ∀ {V₁ V₂ V₃ : Vertex}, GPath G V₁ V₂ → Adjacent G V₂ V₃ → GPath G V₁ V₃
+--Path
+inductive IsPath {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) : Vertex → Vertex → Prop
+| base : ∀ (V : Vertex), IsPath G V V
+| step : ∀ {V₁ V₂ V₃ : Vertex}, IsPath G V₁ V₂ → Adjacent G V₂ V₃ → IsPath G V₁ V₃
 
+--Cycle(*)
+def IsCycle  {Vertex : Type} (V₁ V₂ V₃: Vertex) [DecidableEq Vertex] (G : Graph Vertex) : Prop := 
+    IsPath G V₁ V₂ → IsPath G V₂ V₃ → V₁ = V₃ 
+
+--Connected
 def Connected {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) : Prop :=
   ∀ (V₁ V₂ : Vertex), V₁ ∈ G.Vertices → V₂ ∈ G.Vertices → ∃ (P : GPath G V₁ V₂), true
+
 
 variable (Vertex : Type)
 variable (G : Graph Vertex)
