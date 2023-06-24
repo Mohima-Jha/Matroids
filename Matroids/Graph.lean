99b3ceb697721a62faf6121768e30dc971dcf15e
Mohima <93897060+Mohima-Jha@users.noreply.github.com>
2023-06-24 14:11:42 +0530

    Update Graph.lean

--- Matroids/Graph.lean
+++ Matroids/Graph.lean
@@ -5,8 +5,7 @@ set_option autoImplicit false
 structure Graph (Vertex : Type)where 
   Vertices   : List Vertex
   Edges   : List (Vertex × Vertex)
-  IsWalk : Vertex → Vertex → Prop
-  IsPath : Vertex → Vertex → Prop
+deriving Repr, DecidableEq, Inhabited
 
 --Loop
 def Loop {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) : Prop :=
@@ -30,21 +29,6 @@ inductive IsPath {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) : Verte
 def IsCycle  {Vertex : Type} (V₁ V₂ V₃: Vertex) [DecidableEq Vertex] (G : Graph Vertex) : Prop := 
     IsPath G V₁ V₂ → IsPath G V₂ V₃ → V₁ = V₃ 
 
---Reachable 
-def reachable {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) (u v : Vertex) : Prop :=
-  G.IsWalk u v
-
 --Connected
 def Connected {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) : Prop :=
-  ∀ (V₁ V₂ : Vertex), V₁ ∈ G.Vertices → V₂ ∈ G.Vertices → ∃ (P : GPath G V₁ V₂), true
-
---Ground Set of a Graph is it's edge set
-def Graph_Ground {Vertex : Type} (G : Graph Vertex) : Set (Vertex × Vertex) :=
-  {e | e ∈ G.Edges}
-
-variable (Vertex : Type)
-variable (G : Graph Vertex)
-
-theorem Graphic_Matroid (α : Type _)[DecidableEq α](M : Matroid α): sorry :=
-  sorry 
-
+  ∀ (V₁ V₂ : Vertex), V₁ ∈ G.Vertices → V₂ ∈ G.Vertices → ∃ (P : IsPath G V₁ V₂), true
