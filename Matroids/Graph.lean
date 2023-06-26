ef4d12caebaba759e953aeb104e395ecf49d41d6
Mohima <93897060+Mohima-Jha@users.noreply.github.com>
2023-06-26 16:47:29 +0530

    Update Graph.lean
    Clean Up

--- Matroids/Graph.lean
+++ Matroids/Graph.lean
@@ -2,23 +2,28 @@ import Mathlib
 set_option autoImplicit false
 
 --Graph
-structure Graph (Vertex : Type)where 
+structure Graph (Vertex : Type _) where 
   Vertices   : List Vertex
-  Edges   : List (Vertex × Vertex)
-deriving Repr, DecidableEq, Inhabited
+  Edges      : List (Vertex × Vertex)
+  Adjacent   : Vertex → Vertex → Prop
+  deriving Inhabited
+
+variable {Vertex : Type _}
+variable (G : Graph Vertex) 
 
 --Loop
 def Loop {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) : Prop :=
   ∃ (V : Vertex), V ∈ G.Vertices ∧ (V, V) ∈ G.Edges
 
 --Adjacent
-def Adjacent {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) (V₁ V₂ : Vertex) : Prop :=
-  (V₁,V₂) ∈ G.Edges
+def Adjacent (V₁ V₂ : Vertex): Prop := 
+  (V₁, V₂) ∈ G.Edges ∨ (V₁, V₂) ∈ G.Edges
 
 --Walk
-inductive IsWalk {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) : Vertex → Vertex → Prop
-| base : ∀ (V : Vertex), IsWalk G V V
-| step : ∀ {V₁ V₂ V₃ : Vertex}, IsWalk G V₁ V₂ → Adjacent G V₂ V₃ → IsWalk G V₁ V₃
+inductive IsWalk : Vertex → Vertex → Type u
+  | Base {V₁ : Vertex} : IsWalk V₁ V₁
+  | Step {V₁ V₂ V₃ : Vertex} (h : G.Adjacent V₁ V₂) (p : IsWalk V₂ V₃) : IsWalk V₁ V₃
+attribute [refl] IsWalk.Base 
 
 --Path
 inductive IsPath {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) : Vertex → Vertex → Prop
