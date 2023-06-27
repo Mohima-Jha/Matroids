0e7a7cb53d07cd3c7eac47f1eed3164c1ed3502b
Mohima <93897060+Mohima-Jha@users.noreply.github.com>
2023-06-27 12:22:26 +0530

    Update Graph.lean

--- Matroids/Graph.lean
+++ Matroids/Graph.lean
@@ -18,7 +18,7 @@ def Loop {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) : Prop :=
 
 --Adjacent
 def Adjacent (V₁ V₂ : Vertex): Prop := 
-  (V₁, V₂) ∈ G.Edges ∨ (V₁, V₂) ∈ G.Edges
+  (V₁, V₂) ∈ G.Edges
 
 --Walk
 inductive IsWalk : Vertex → Vertex → Type u
@@ -26,15 +26,13 @@ inductive IsWalk : Vertex → Vertex → Type u
   | Step {V₁ V₂ V₃ : Vertex} (h : G.Adjacent V₁ V₂) (p : IsWalk V₂ V₃) : IsWalk V₁ V₃
 attribute [refl] IsWalk.Base 
 
---Path
-inductive IsPath {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) : Vertex → Vertex → Prop
-| base : ∀ (V : Vertex), IsPath G V V
-| step : ∀ {V₁ V₂ V₃ : Vertex}, IsPath G V₁ V₂ → Adjacent G V₂ V₃ → IsPath G V₁ V₃
-
---Cycle(*)
-def IsCycle  {Vertex : Type} (V₁ V₂ V₃: Vertex) [DecidableEq Vertex] (G : Graph Vertex) : Prop := 
-    IsPath G V₁ V₂ → IsPath G V₂ V₃ → V₁ = V₃ 
+/-
+inductive IsPath : Vertex → Vertex → Type _
+  |Base {V : Vertex} : IsPath V V
+  |Adjacent {V₁ V₂ : Vertex} : G.Adjacent V₁ V₂ → IsWalk V₁ V₂
+  |Step {V₁ V₂ V₃ : Vertex}(h : G.Adjacent V₁ V₂) (p : IsWalk V₂ V₃) (Something here to make sure unique Vertices) : IsPath V₁ V₃
+-/
 
 --Connected
-def Connected {Vertex : Type} [DecidableEq Vertex] (G : Graph Vertex) : Prop :=
-  ∀ (V₁ V₂ : Vertex), V₁ ∈ G.Vertices → V₂ ∈ G.Vertices → ∃ (P : IsPath G V₁ V₂), true
+def Connected (V₁ V₂ : Vertex) : Prop := Nonempty (IsWalk G V₁ V₂)
+
